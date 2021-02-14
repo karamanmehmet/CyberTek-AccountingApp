4ebfd8708ddec07aee4a598ed4ea208db2eebd85
Mehmet KARAMAN <karamanmehmet@yahoo.com>
2021-02-13 23:07:10 -0500

    Initial commit

---
+++ src/main/java/com/cybertek/accounting/AccountingAppApplication.java
@@ -0,0 +1,13 @@
+package com.cybertek.accounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountingAppApplication.class, args);
+	}
+
+}
