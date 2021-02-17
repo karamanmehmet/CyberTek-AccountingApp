cd9f230ca1975d07da151be496f912dc2533fb83
Mehmet <mhmtsytrk@gmail.com>
2021-02-17 22:47:37 +0300

    SPTable added

--- src/main/java/com/cybertek/accounting/entity/SPTable.java
+++ src/main/java/com/cybertek/accounting/entity/SPTable.java
@@ -1,18 +1,18 @@
 package com.cybertek.accounting.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Entity
 @Setter
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "sp_tables")
 public class SPTable extends BaseEntity {
 
     @Column(unique = true,nullable = false)
@@ -25,7 +25,7 @@ public class SPTable extends BaseEntity {
 
     private String type;
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name="company_id")
     private Company company;
 
