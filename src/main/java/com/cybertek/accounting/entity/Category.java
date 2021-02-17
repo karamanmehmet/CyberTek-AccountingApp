d70821f79de8dedad1f8c10bfec1414850c9164c
CundullahT <cundullahterzioglu.it@gmail.com>
2021-02-18 00:07:53 +0100

    ProductRepositoryTest and CategoryRepositoryTest

--- src/main/java/com/cybertek/accounting/entity/Category.java
+++ src/main/java/com/cybertek/accounting/entity/Category.java
@@ -8,18 +8,18 @@ import lombok.Setter;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "categories")
+@Table(name = "category")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Category extends BaseEntity {
 
-    @Column(name = "description", updatable = true, nullable = false)
-    public String description;
+    @Column(name = "description", nullable = false)
+    private String description;
 
     @ManyToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "company_id", nullable = false)
-    public Company company;
+    private Company company;
 
 }
---
+++ src/test/java/com/cybertek/accounting/repository/CategoryRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.cybertek.accounting.repository;
+
+import com.cybertek.accounting.entity.Category;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(value = false)
+public class CategoryRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    public void testCreateCategory() {
+
+        Category category1 = new Category("Electronics", companyRepository.findById(1).get());
+        Category category2 = new Category("Clothing", companyRepository.findById(2).get());
+        Category category3 = new Category("Food & Drink", companyRepository.findById(3).get());
+
+        entityManager.persist(category1);
+        entityManager.persist(category2);
+        entityManager.persist(category3);
+
+    }
+
+}
