b09f96600d6e10cce206edf53ec327adc6671fe7
CundullahT <cundullahterzioglu.it@gmail.com>
2021-02-18 20:16:58 +0100

    ProductDto and CategoryDto

---
+++ src/main/java/com/cybertek/accounting/dto/ProductDto.java
@@ -0,0 +1,27 @@
+package com.cybertek.accounting.dto;
+
+import com.cybertek.accounting.enums.Status;
+import com.cybertek.accounting.enums.Unit;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDto {
+
+    private long id;
+    private String name;
+    private String description;
+    private int qty;
+    private double price;
+    private CategoryDto category;
+    private Unit unit;
+//    private double tax;       ???
+    private CompanyDto company;
+    private Status status;
+
+}
