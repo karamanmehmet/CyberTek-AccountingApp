acf10824a5becb736e9a0268c27986a38d5091e4
Mehmet <mhmtsytrk@gmail.com>
2021-02-19 22:28:28 +0300

    SPTableRepository and SPTableService are Updated

--- src/main/java/com/cybertek/accounting/repository/SPTableRepository.java
+++ src/main/java/com/cybertek/accounting/repository/SPTableRepository.java
@@ -17,11 +17,19 @@ public interface SPTableRepository extends JpaRepository<SPTable,Long> {
 
     List<SPTable> findAllByCompany(Company company);
 
+    List<SPTable> findAllByCompanyAndEnabled(Company company,boolean enabled);
+
     List<SPTable> findAllByType(String type);
 
-    List<SPTable> findAllByEmail(String email);
+    List<SPTable> findAllByTypeAndEnabled(String type,boolean enabled);
+
+    Optional<SPTable> findByEmail(String email);
+
+    Optional<SPTable> findAllByEmailAndEnabled(String email,boolean enabled);
 
     List<SPTable> findAllByState(String state);
+
+    List<SPTable> findAllByStateAndEnabled(String state,boolean enabled);
 
     List<SPTable> findAllByCompanyAndStatusAndType(Company company, Status status, String type);
 
