92f2b6dbe7d3fe395c4fb5ed882a7da942dd5c91
Mehmet <mhmtsytrk@gmail.com>
2021-02-21 15:21:42 +0300

    ClientVendorType Enum added

---
+++ src/main/java/com/cybertek/accounting/enums/ClientVendorType.java
@@ -0,0 +1,12 @@
+package com.cybertek.accounting.enums;
+
+public enum ClientVendorType {
+    CLIENT("Client"),VENDOR("Vendor");
+    private final String value;
+    private ClientVendorType(String value){
+        this.value = value;
+    }
+    public String getValue(){
+        return value;
+    }
+}
