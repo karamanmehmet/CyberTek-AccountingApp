9e3e16917ce8df7d6d8331aa303decc0f323c6d8
fatmaoz <fatmas199021@gmail.com>
2021-02-22 14:47:06 +0000

    Security disabled

--- src/main/java/com/cybertek/accounting/AccountingAppApplication.java
+++ src/main/java/com/cybertek/accounting/AccountingAppApplication.java
@@ -10,13 +10,14 @@ import com.cybertek.accounting.repository.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 
 
-@SpringBootApplication
+@SpringBootApplication//(exclude = {SecurityAutoConfiguration.class})
 public class AccountingAppApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/cybertek/accounting/configuration/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.cybertek.accounting.configuration;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/**")//postmanda yazdiginda token sorulmicak
+                .permitAll();
+
+
+    }
+}
