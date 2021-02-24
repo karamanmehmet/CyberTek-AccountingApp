bf3ca185938c4c23a38a74db25c9bf86d7197980
fatmaoz <fatmas199021@gmail.com>
2021-02-24 22:48:00 +0000

    RoleRestController

--- src/main/java/com/cybertek/accounting/restcontroller/RoleRestController.java
+++ src/main/java/com/cybertek/accounting/restcontroller/RoleRestController.java
@@ -1,9 +1,13 @@
 package com.cybertek.accounting.restcontroller;
 
+import com.cybertek.accounting.dto.RoleDto;
+import com.cybertek.accounting.entity.Role;
 import com.cybertek.accounting.mapper.MapperGeneric;
 import com.cybertek.accounting.service.RoleService;
 import com.cybertek.accounting.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class RoleRestController {
     private final RoleService roleService;
     private final MapperGeneric mapper;
+
+    @GetMapping("/{name}")
+    RoleDto findByName(@PathVariable("name") String name){
+        return roleService.findByName(name);
+    }
 
 }
